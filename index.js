69522514c967b3eec14f354d4546c2cc1da53f13
eternal-sahil <eternalsahil1999@gmail.com>
2025-03-18 09:54:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+// index.js
+require("dotenv").config();
+const express = require("express");
+const bot = require("./bot");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// A simple route to confirm the service is running.
+app.get("/", (req, res) => {
+  res.send("Telegram Bot is running.");
+});
+
+// Start Express server
+app.listen(PORT, () => {
+  console.log(`Express server is running on port ${PORT}`);
+});
